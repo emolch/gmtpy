3635bbc3ddafd3b1b66e560512e6ee93ae6c5518
Sebastian Heimann <sebastian.heimann@zmaw.de>
2011-02-17 08:19:02 +0000

    setup.py will now install gmtpy-eptopdf

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup( name    = 'gmtpy',
        version = '0.1',
        description = 'Thin GMT interface plus autoscaling plus layout management.',
        py_modules = ['gmtpy'],
+       scripts = [ 'gmtpy-epstopdf' ],
        author = 'Sebastian Heimann',
        author_email = 'sebastian.heimann@zmaw.de',
        )
