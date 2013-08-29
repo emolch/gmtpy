1c709253ad67521ada151e8ca0fdb74546a655fe
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2013-08-29 09:59:20 +0200

    added link to docs page in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
-setup( name    = 'gmtpy',
-       version = '0.1',
-       description = 'Thin GMT interface plus autoscaling plus layout management.',
-       py_modules = ['gmtpy'],
-       scripts = [ 'gmtpy-epstopdf' ],
-       author = 'Sebastian Heimann',
-       author_email = 'sebastian.heimann@zmaw.de',
-       )
+setup( 
+    name    = 'gmtpy',
+    version = '0.1',
+    description = 'Thin GMT interface plus autoscaling plus layout management.',
+    py_modules = ['gmtpy'],
+    scripts = [ 'gmtpy-epstopdf' ],
+    author = 'Sebastian Heimann',
+    author_email = 'sebastian.heimann@zmaw.de',
+    url = 'http://emolch.github.com/gmtpy/',
+
+)
